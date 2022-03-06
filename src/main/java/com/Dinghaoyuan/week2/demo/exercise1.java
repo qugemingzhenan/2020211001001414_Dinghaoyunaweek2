d566edcb23a0afd0fb40707c1eb05a874faa236a
dinhaoyuan <741104712@qq.com>
2022-03-06 10:55:36 +0800

    Week 2 Commit

---
+++ src/main/java/com/Dinghaoyuan/week2/demo/exercise1.java
@@ -0,0 +1,23 @@
+package com.Dinghaoyuan.week2.demo;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class exercise1 extends HttpServlet{
+    public void doGet(HttpServletRequest request,HttpServletResponse response)
+    throws IOException {
+        PrintWriter writer= response.getWriter();
+        writer.println("Name: Dinghaoyuan");
+        writer.println("ID: 2020211001001414");
+        writer.println("Date and Time: Sun March 6 10:47");
+
+
+    }
+    public void doPost(HttpServletRequest request,HttpServletResponse response){
+
+    }
+
+}
