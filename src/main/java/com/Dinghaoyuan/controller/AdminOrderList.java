f5db657fc82f21006f32b14adaf67976e7d8ccb0
dinhaoyuan <741104712@qq.com>
2022-06-03 15:47:27 +0800

    week 15

---
+++ src/main/java/com/Dinghaoyuan/controller/AdminOrderList.java
@@ -0,0 +1,42 @@
+package com.Dinghaoyuan.controller;
+
+import com.Dinghaoyuan.dao.OrderDao;
+import com.Dinghaoyuan.model.Order;
+import com.Dinghaoyuan.model.Payment;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.List;
+
+@WebServlet("/admin/orderList")
+public class AdminOrderList extends HttpServlet {
+    private Connection con = null;
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+    @Override
+    public void init() throws ServletException {
+        con = (Connection) getServletContext().getAttribute("con");
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Payment> paymentTypeList =Payment.findAllPayment(con);
+        request.setAttribute("paymentTypeList",paymentTypeList);
+        OrderDao orderDao = new OrderDao();
+        List<Order> orderList = orderDao.findAll(con);
+        request.setAttribute("orderList",orderList);
+        String path="/WEB-INF/views/admin/orderList.jsp";
+        request.getRequestDispatcher(path).forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
